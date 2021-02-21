15faf00eef19c700b77c68ae9c9db8f0b2057df5
xkostrna <xkostrna@stuba.sk>
2021-02-21 16:23:21 +0100

    Hello_World function added

---
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.oop;
+
+public class Main {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello_World");
+    }
+
+    public static void main(String[] args) {
+       printHelloWorld();
+    }
+}
