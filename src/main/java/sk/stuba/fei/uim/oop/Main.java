20f199901a01ac64c8aa4b95963df7f36bef810c
xkostrna <xkostrna@stuba.sk>
2021-02-21 16:45:51 +0100

    Single dimensional array initializer function added

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,6 +1,9 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void printHelloWorld() {
         System.out.println("Hello_World");
@@ -21,8 +24,30 @@ public class Main {
         System.out.println(compareTwoInts(5,6));
     }
 
-    public static void main(String[] args) {
+    public static int[] createSingleDimensionalArray(int arraySize) throws Exception {
+        if (arraySize == 0) {
+            throw new Exception("Wrong array size");
+        }
+
+        int[] newArray = new int[arraySize];
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print("Enter " + i + " value : ");
+            newArray[i] = scanner.nextInt();
+        }
+        return newArray;
+    }
+
+    public static void printArray(int[] arrayToPrint) {
+        for(int number : arrayToPrint) {
+            System.out.print(number + " ");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         printHelloWorld();
         testCompareTwoInts();
+        int[] arrayOfInts = createSingleDimensionalArray(5);
+        printArray(arrayOfInts);
+
     }
 }
