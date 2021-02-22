f8de47c2b770370974e0bd27b43bb0e0de1e0d67
xkostrna <xkostrna@stuba.sk>
2021-02-22 09:37:42 +0100

    Two dimensional array initializer added. Determinant calculator added.

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static final int THROW_VALUE = 0;
     public static Scanner scanner = new Scanner(System.in);
 
     public static void printHelloWorld() {
@@ -25,8 +26,8 @@ public class Main {
         System.out.println(compareTwoInts(5,6));
     }
 
-    public static int[] createSingleDimensionalArray(int arraySize) throws Exception {
-        if (arraySize == 0) {
+    public static int[] createSingleDimensionalArray(final int arraySize) throws Exception {
+        if (arraySize == THROW_VALUE) {
             throw new Exception("Wrong array size");
         }
         return new int[arraySize];
@@ -45,17 +46,81 @@ public class Main {
         }
     }
 
-    public static void printArray(int[] arrayToPrint) {
+    public static void printArray(final int[] arrayToPrint) {
         for(int number : arrayToPrint) {
             System.out.print(number + " ");
         }
         System.out.println();
     }
 
+    public static int[][] createTwoDimensionalArray(final int ROWS, final int COLUMNS) throws Exception {
+        if (ROWS == THROW_VALUE || COLUMNS == THROW_VALUE) {
+            throw new Exception("Wrong array size");
+        }
+        return new int[ROWS][COLUMNS];
+    }
+
+    public static void keyboardFillTwoDimArr(int[][] arrayToFill) {
+        for (int i = 0; i < arrayToFill.length; i++) {
+            for (int j = 0; j < arrayToFill[i].length; j++) {
+                System.out.print("Add number to " + i + ". row " +j+ ". column : ");
+                arrayToFill[i][j] = scanner.nextInt();
+            }
+        }
+    }
+
+    public static void randomFillTwoDimArr(int[][] arrayToFill) {
+        for (int[] rows : arrayToFill) {
+            Arrays.fill(rows, (int) (Math.random()*10));
+        }
+    }
+
+    public static void printTwoDimArr(final int[][] arrayToPrint) {
+        for(int[] rows : arrayToPrint) {
+            System.out.println();
+            for(int number : rows) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int determinateMatrix(final int[][] arrayToCount) {
+        int[][] tempArray;
+        int result = 0;
+
+        if (arrayToCount.length == 1) {
+            result = arrayToCount[0][0];
+            return result;
+        }
+
+        if (arrayToCount.length == 2) {
+            result = ((arrayToCount[0][0] * arrayToCount[1][1]) - (arrayToCount[0][1] * arrayToCount[1][0]));
+            return result;
+        }
+
+        for (int i = 0; i < arrayToCount[0].length; i++) {
+            tempArray = new int[arrayToCount.length -1][arrayToCount[0].length -1];
+
+            for (int j = 1; j < arrayToCount.length; j++) {
+
+                for (int k = 0; k < arrayToCount[0].length; k++) {
+                    if(k < i) {
+                        tempArray[j-1][k] = arrayToCount[j][k];
+                    }
+                    else if(k > i) {
+                        tempArray[j-1][k-1] = arrayToCount[j][k];
+                    }
+                }
+            }
+            result += arrayToCount[0][i] * Math.pow(-1, i) * determinateMatrix(tempArray);
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws Exception {
         printHelloWorld();
         testCompareTwoInts();
-
 
         final int ARRAY_SIZE = 5;
         int[] keyboardArray = createSingleDimensionalArray(ARRAY_SIZE);
@@ -67,6 +132,15 @@ public class Main {
         Arrays.sort(randomArray);
         printArray(randomArray);
 
+        int[][] randomTwoDimArray = createTwoDimensionalArray(3,10);
+        randomFillTwoDimArr(randomTwoDimArray);
+        printTwoDimArr(randomTwoDimArray);
+
+        int[][] keyboardTwoDimArray = createTwoDimensionalArray(3,3);
+        keyboardFillTwoDimArr(keyboardTwoDimArray);
+        printTwoDimArr(keyboardTwoDimArray);
+        int determinantOfArray = determinateMatrix(keyboardTwoDimArray);
+        System.out.println(determinantOfArray);
 
     }
 }
